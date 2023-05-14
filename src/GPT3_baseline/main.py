7e11985dacc9f5f66e59a874052de2794342c0d4
Sam Coan <62761327+slxmmy999@users.noreply.github.com>
2023-05-14 15:24:52 -0600

    Update main.py

--- src/GPT3_baseline/main.py
+++ src/GPT3_baseline/main.py
@@ -1,6 +1,6 @@
 import openai
 
-openai.api_key = "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"
+openai.api_key = ""
 
 messages=[
     {"role": "system", "content": "You are a helpful and friendly language model. Your job is to analyze user input and recommend music based on their input. If a message is not music related, find a way to relate it to a song and recommend that song to the user."}
